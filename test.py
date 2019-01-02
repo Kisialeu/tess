7d60636e739a66f8ff72216c2850cacf29c95a0a
Kisialeu <kisialeu@gmail.com>
2019-01-02 16:18:17 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+import pytest
+
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(3) == 4
+
+
+def f():
+    raise SystemExit(1)
+
+def test_mytest():
+    with pytest.raises(SystemExit):
+
+        f()
+def funco(x):
+    return x + 1
+
+def test_answer2():
+    assert funco(3) == 4
