da2e0c8d7bdd14b501876d94af1351d417c34940
Kisialeu <kisialeu@gmail.com>
2019-01-02 16:51:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -22,3 +22,21 @@ def test_answer2():
 
 def test_negative():
     assert funco(10) == 500
+    
+def test_success():
+    """this test succeeds"""
+    assert True
+
+
+def test_failure():
+    """this test fails"""
+    assert False
+
+
+def test_skip():
+    """this test is skipped"""
+    pytest.skip('for a reason!')
+
+
+def test_broken():
+    raise Exception('oops')
